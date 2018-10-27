352ed6e53197ebc17e49dc90ec2f58551af15ee4
Ron Compos <rcompos@gmail.com>
2018-10-27 17:27:12 -0600

    Function values go practice.

---
+++ function_values.go
@@ -0,0 +1,22 @@
+// _Function values_ 
+
+package main
+
+import "fmt"
+
+func square(n int) int { return n * n }
+func negative(n int) int { return -n }
+func product(m, n int) int { return m * n }
+
+func main() {
+
+	f := square
+	fmt.Println(f(3))
+
+	f = negative
+	fmt.Println(f(3))
+	fmt.Printf("%T\n", f)
+
+	//f = product
+
+}
--- string-to-num.go
+++ string-to-num.go
@@ -3,11 +3,32 @@ package main
 import(
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
+
 	fmt.Println("Hello wurld!")
 	x := 123
 	y := fmt.Sprintf("%d", x)
 	fmt.Println(y, strconv.Itoa(x))
+
+	fmt.Println(strconv.FormatInt(int64(x), 2))
+
+	s := fmt.Sprintf("x=%b", x)
+	fmt.Println(s)
+
+	//xx, errXx := strconv.Atoi("123")
+	//fmt.Println(xx,errXx)
+	xx, _ := strconv.Atoi("123")
+	fmt.Println(xx)
+	yy, errYy := strconv.ParseInt("123", 10, 64)
+	fmt.Println(yy,errYy)
+
+	const noDelay time.Duration = 0
+	const timeout = 5 * time.Minute
+	fmt.Printf("%T %[1]v\n", noDelay)
+	fmt.Printf("%T %[1]v\n", timeout)
+	fmt.Printf("%T %[1]v\n", time.Minute)
+
 }
