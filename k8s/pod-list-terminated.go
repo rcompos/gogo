13fc32d8108aaa748a1f2e04e8814d510dfbd34d
Ron Compos <rcompos@cornholio.local>
2019-01-22 08:57:35 -0700

    Added go-client code.

--- k8s/pod-list-terminated.go
+++ k8s/pod-list-terminated.go
@@ -1,11 +1,13 @@
 package main
 
-import (
-	"flag"
+import ( "flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	//"regexp"
+	"encoding/json"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -36,24 +38,47 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	pods, err := clientset.CoreV1().Pods(ns).List(metav1.ListOptions{})
-	//pods, err := clientset.CoreV1().Pods(ns).List(metav1.ListOptions{
-	//	LabelSelector: "function=isprime",
-	//})
+	namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
 	if err != nil {
-		log.Fatalln("failed to get pods:", err)
+		log.Fatalln("failed to get namespaces:", err)
 	}
 
-	// print pods
-	//for i, pod := range pods.Items {
-	//	fmt.Printf("[%d] %s\n", i, pod.GetName())
-	//}
-	for _, pod := range pods.Items {
-		//fmt.Println(pod.Name, pod.Status.PodIP)
-		//fmt.Println(pod.Name, pod.Status)
-		//fmt.Println(pod.Name, pod.Status.ContainerStatuses)
-		fmt.Println(pod.Name, pod.Status.ContainerStatuses)
-		//fmt.Println(pod.Name, pod.Status.Phase)
+	for _, namespace := range namespaces.Items {
+		fmt.Println("####  ", namespace.Name, "  ####")
+		//pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+		//pods, err := clientset.CoreV1().Pods(ns).List(metav1.ListOptions{})
+		pods, err := clientset.CoreV1().Pods(string(namespace.Name)).List(metav1.ListOptions{})
+		//pods, err := clientset.CoreV1().Pods(ns).List(metav1.ListOptions{
+		//	LabelSelector: "function=isprime",
+		//})
+		if err != nil {
+			log.Fatalln("failed to get pods:", err)
+		}
+
+		// print pods
+		//for i, pod := range pods.Items {
+		//	fmt.Printf("[%d] %s\n", i, pod.GetName())
+		//}
+		//reggie := regexp.MustCompile(`.*Terminated.*`)
+		for _, pod := range pods.Items {
+			//fmt.Println(pod.Name, pod.Status.PodIP)
+			//fmt.Println(pod.Name, pod.Status)
+			//fmt.Println(pod.Name, pod.Status.ContainerStatuses)
+			//fmt.Printf("%q\n", reggie.FindString(pod.Status.ContainerStatuses))
+			//fmt.Printf("%q\n", reggie.MatchString("asdfaTrminated kwik"))
+			b := pod.Status.ContainerStatuses
+			//a := &b
+		    out, err := json.Marshal(b)
+			if err != nil {
+				panic (err)
+			}
+			//fmt.Println(string(out))
+			fmt.Println("OUT: ", string(out))
+			//fmt.Println(a)
+			//fmt.Println(b)
+			fmt.Println(strings.Contains(string(out), "terminated"))
+			//fmt.Printf(strings.Contains(pod.Status.ContainerStatuses, "Terminated"))
+			//fmt.Printf("%s\n", strings.Contains(out, "Terminated"))
+		}
 	}
 }
